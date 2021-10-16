6a6fb079309e7c7e616efc39f4593487a89f91f3
github username <github email address>
2021-10-15 20:10:07 -0500

    commit tree 99d01468b84dc0ca115004d7904434f6ea1c8ae2

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (pickup) {
+  return Math.abs(pickup - 42) 
+ }
+ 
+
+function distanceFromHqInFeet (pickup) {
+ const blocks = distanceFromHqInBlocks(pickup) 
+ return blocks * 264;
+}
+
+function distanceTravelledInFeet (start, end) {
+   const distanceTravelled = Math.abs(end - start)
+   return distanceTravelled * 264;
+
+}
+
+ function calculatesFarePrice (start, end){
+      const traveld = distanceTravelledInFeet (start, end)
+     if (traveld  < 400) {
+
+        return 0;
+
+     } else if (traveld > 400 && traveld  < 2000){
+
+      return (traveld  - 400) * 2 / 100;
+
+     }else if (traveld > 2000 && traveld  < 2500){
+
+      return 25;
+
+     }else if (traveld  > 2500) {
+
+      return 'cannot travel that far'
+
+     }
+
+ }
